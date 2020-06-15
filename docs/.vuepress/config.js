02423bff71a30eda27335ef48d1cc1d925e7dda2
18310957475 <18310957475@163.com>
2020-06-15 10:20:18 +0800

    第一次提交配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+    title: '胡剑锋的博客1',
+    description: '我的个人博客',
+    base: '/',
+    themeConfig: {
+        navbar: false,
+        sidebar: 'auto',
+        sidebarDepth: 3,
+    }
+}
+
